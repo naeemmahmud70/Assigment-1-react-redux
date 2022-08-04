a3887c7a41bf2aafed2efef001578467ce007723
Naeem Miah <naeem@sols247.org>
2022-08-04 13:06:45 +0600

    readme file edited

--- src/components/Login/Login/Login.js
+++ src/components/Login/Login/Login.js
@@ -53,55 +53,59 @@ const Login = () => {
           </div>
         </div>
 
-        <div className="col-md-5 d-flex justify-content-center">
-          <div className="login-form">
-            <form onSubmit={handleSubmit(onSubmit)} action="">
-              <div className="form-group">
-                <input
-                  name="mobileOrEmail"
-                  placeholder="Email adress or phone number"
-                  className="form-control login-input"
-                  type="text"
-                  required
-                  {...register("mobileOrEmail")}
-                />
-              </div>
-              <div className="form-group mt-3">
-                <input
-                  name="password"
-                  placeholder="passsword"
-                  className="form-control login-input"
-                  type="password"
-                  required
-                  {...register("password")}
-                />
-              </div>
-              <div className="mt-3">
-                <button type="submit" className="login-btn w-100 fw-bold">
-                  Login
-                </button>
-              </div>
-            </form>
-            <p className="text-primary text-center mt-3">Forgotten password?</p>
-            <hr />
+        <div className="col-md-5 ">
+          <div className="d-flex justify-content-center">
+            <div className="login-form">
+              <form onSubmit={handleSubmit(onSubmit)} action="">
+                <div className="form-group">
+                  <input
+                    name="mobileOrEmail"
+                    placeholder="Email adress or phone number"
+                    className="form-control login-input"
+                    type="text"
+                    required
+                    {...register("mobileOrEmail")}
+                  />
+                </div>
+                <div className="form-group mt-3">
+                  <input
+                    name="password"
+                    placeholder="passsword"
+                    className="form-control login-input"
+                    type="password"
+                    required
+                    {...register("password")}
+                  />
+                </div>
+                <div className="mt-3">
+                  <button type="submit" className="login-btn w-100 fw-bold">
+                    Login
+                  </button>
+                </div>
+              </form>
+              <p className="text-primary text-center mt-3">
+                Forgotten password?
+              </p>
+              <hr />
 
-            <div>
-              <div className="d-flex justify-content-center">
-                <button
-                  type="button"
-                  onClick={openModal}
-                  className="btn create-btn text-white fw-bold p-2"
-                >
-                  Create New Account
-                </button>
-                <Registration
-                  modalIsOpen={modalIsOpen}
-                  closeModal={closeModal}
-                ></Registration>
+              <div>
+                <div className="d-flex justify-content-center">
+                  <button
+                    type="button"
+                    onClick={openModal}
+                    className="btn create-btn text-white fw-bold p-2"
+                  >
+                    Create New Account
+                  </button>
+                  <Registration
+                    modalIsOpen={modalIsOpen}
+                    closeModal={closeModal}
+                  ></Registration>
+                </div>
               </div>
             </div>
           </div>
-          <p className=" mt-4">
+          <p className="text-center mt-4">
             <strong>Create a page</strong> for a celebraty, brands or buisiness.
           </p>
         </div>
